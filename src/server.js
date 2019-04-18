f16836d608b5e2d2527a9844b406e125def988d2
Lucas Wanderley <lucaswanderley4@gmail.com>
2019-04-17 22:01:46 -0300

    Adicionado rota para acessar arquivos via url

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const path = require("path")
 
 const app = express()
-
-app.use(express.json()) //lidar com json
-app.use(express.urlencoded({ extended: true })) //permite envio de arquivos
 
 mongoose.connect("mongodb+srv://lucas:asjhbf10@cluster0-jliyv.mongodb.net/omnistack?retryWrites=true", {
   useNewUrlParser: true,
 })
+
+app.use(express.json()) //lidar com json
+app.use(express.urlencoded({ extended: true })) //permite envio de arquivos
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp'))) // rota para acessar os arquivos que foram adicionados
+
+
 
 app.use(require('./routes'))
 
